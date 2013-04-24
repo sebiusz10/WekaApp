e6e18da7dccb8ddecf4fa1d520713c45de1909d8
sebiusz10 <sebiusz10@o2.pl>
2013-04-24 22:24:20 +0200

    Wczytywanie pliku arff
    Zostało dokończyć wyświetlanie bo żeby ładnie to wyglądało stwierdziłem,
    że lepiej będzie wyświetlać te dane w JTable

---
+++ WekaApp/src/wekaapp/Main.java
@@ -0,0 +1,169 @@
+
+package wekaapp;
+import com.sun.xml.internal.ws.api.message.Message;
+import java.awt.FileDialog;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import javax.swing.JTable;
+import javax.swing.table.TableColumn;
+import weka.core.Attribute;
+
+/**
+ *
+ * @author sgolebiewski
+ */
+public class Main extends javax.swing.JFrame 
+{
+
+    Instances data = null;
+    DataSource source = null;
+    JTable table = null;
+    JScrollPane scrollpane = null;
+    
+    public Main() {
+        initComponents();
+        table = new JTable();
+        scrollpane = new JScrollPane(table);
+        scrollpane.setLocation(10, 110);
+        scrollpane.setSize(610, 240);
+        this.add(scrollpane);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jTextField1 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTextField1.setName("txtPath");
+
+        jLabel1.setText("Plik arff lub csv.");
+
+        jButton1.setText("Przeglądaj");
+        jButton1.setName("btnBrowse");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Zródło:");
+
+        jButton2.setText("Wczytaj");
+        jButton2.setName("btnBrowse");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 457, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
+                .addContainerGap(268, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        
+        FileDialog fd =new FileDialog(this,"Wczytaj",FileDialog.LOAD);
+   
+        fd.setVisible(true);
+        String sDirectory=fd.getDirectory();
+        String sFile=fd.getFile();
+        
+        this.jTextField1.setText(sDirectory + "\\" + sFile);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        
+        try {
+            source = new DataSource(this.jTextField1.getText());
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            data = source.getDataSet();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        if (data.classIndex() == -1)
+        data.setClassIndex(data.numAttributes() - 1);
+        
+        if(source != null)
+        {
+            JOptionPane.showMessageDialog(this,"Plik wczytany!");
+        }
+        
+        //int countAtr = data.numAttributes();
+        
+        //String [] tbl = new String[countAtr];
+        
+        //for(int i=0;i<countAtr;i++)
+        //{
+           
+        //}
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                new Main().setVisible(true);
+            }
+        });
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ WekaApp/src/wekaapp/WekaApp.java
@@ -0,0 +1,17 @@
+package wekaapp;
+
+/**
+ *
+ * @author sgolebiewski
+ */
+public class WekaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.setVisible(true);
+        main.show();
+    }
+}
