ca1196c602c88abf2706b4a12b78390a18323ae0
sebiusz10 <sebiusz10@o2.pl>
2013-05-03 21:00:22 +0200

    fixes

--- WekaApp/src/wekaapp/WekaApp.java
+++ WekaApp/src/wekaapp/WekaApp.java
@@ -1,14 +1,12 @@
 package wekaapp;
 
-<<<<<<< HEAD
 import wekaapp.ui.Main;
-=======
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
->>>>>>> 680249ef934480be9ac1e17b1373cb4474ed275f
 
 /**
  *
--- WekaApp/src/wekaapp/ui/Main.java
+++ WekaApp/src/wekaapp/ui/Main.java
@@ -220,8 +220,11 @@ public class Main extends javax.swing.JFrame
 <<<<<<< HEAD:WekaApp/src/wekaapp/ui/Main.java
 <<<<<<< HEAD:WekaApp/src/wekaapp/ui/Main.java
 <<<<<<< HEAD:WekaApp/src/wekaapp/ui/Main.java
+<<<<<<< HEAD:WekaApp/src/wekaapp/ui/Main.java
             lsOptions.add("-U");
 =======
+=======
+>>>>>>> parent of 680249e... Add cofidence option -C:WekaApp/src/wekaapp/Main.java
 =======
 >>>>>>> parent of 680249e... Add cofidence option -C:WekaApp/src/wekaapp/Main.java
 =======
@@ -239,6 +242,7 @@ public class Main extends javax.swing.JFrame
 <<<<<<< HEAD:WekaApp/src/wekaapp/ui/Main.java
 <<<<<<< HEAD:WekaApp/src/wekaapp/ui/Main.java
 <<<<<<< HEAD:WekaApp/src/wekaapp/ui/Main.java
+<<<<<<< HEAD:WekaApp/src/wekaapp/ui/Main.java
             lsOptions.add("-M");
             lsOptions.add(this.txtMinimal.getText());
         }
@@ -249,6 +253,15 @@ public class Main extends javax.swing.JFrame
             lsOptions.add(this.txtConfidence.getText());
         }
         
+=======
+            if(lsOptions == null)
+            {
+                lsOptions = new ArrayList<String>();
+            }
+            lsOptions.add("M" + this.txtMinimal.getText());
+        }
+        
+>>>>>>> parent of 680249e... Add cofidence option -C:WekaApp/src/wekaapp/Main.java
 =======
             if(lsOptions == null)
             {
